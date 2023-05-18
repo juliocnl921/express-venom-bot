f55bfbc08c910d7990f4dc8d78b360d4251e2e1b
julio <julio69413@gmail.com>
2023-05-18 10:50:44 -0700

    ajustes en parametros en los endpoints de main.js

--- main.js
+++ main.js
@@ -1,39 +1,25 @@
 const express = require('express')
 const sender = require('./sender');
+
 const app = express();
 app.use(express.json({limit: "50mb"}));
 
 const port = 3000;
 
 app.get('/enviar', async (req, res) => {
-  let phone = req.query.phone;
-  let message = req.query.message;
-
-  const result = await sender.sendText(phone, message)
+  const result = await sender.sendText(req.query)
 
   res.send(result);
 });
 
 app.post('/send_whatsapp', async (req, res) => {
-  let phone = req.body.phone;
-  let message = req.body.message;
-  console.log(">>>>>>>>>>>>>phone>",phone)
-  phone = phone.replace("-", "").replace(" ", "").replace("(", "").replace(")", "").replace("+", "")
-
-  const result = await sender.sendText(phone, message)
+  const result = await sender.sendText(req.body)
 
   res.send(result);
 });
 
 app.post('/send_whatsapp_with_file', async (req, res) => {
-  let phone = req.body.phone;
-  let message = req.body.message;
-  let file = req.body.file;
-  let file_name = req.body.file_name;
-
-  phone = phone.replace("-", "").replace(" ", "").replace("(", "").replace(")", "").replace("+", "")
-
-  const result = await sender.sendFileFromBase64(phone, file, file_name, message)
+  const result = await sender.sendFileFromBase64(req.body)
 
   res.send(result);
 });
